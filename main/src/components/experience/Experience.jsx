84166b1fceee2d91527e175e28802f59f4abc9be
Justin 석원 Choi <justinschoi93@gmail.com>
2024-02-25 19:26:00 -0800

    created elements. Still need signature.

--- main/src/components/experience/Experience.jsx
+++ main/src/components/experience/Experience.jsx
@@ -1,5 +1,41 @@
-function Experience () {
+import React from "react";
+import "./experience.css";
+import { BsCheck2Circle } from "react-icons/bs";
 
+function Experience () {
+    return (
+        <section id="experience">
+            <h5>Experience</h5>
+            <h2>Acquired Skills</h2>
+
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>Front-end Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>HTML5 (Semantic)</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div className="experience__backend">
+                    <h3>Back-end Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Node.js</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 
 }
 
