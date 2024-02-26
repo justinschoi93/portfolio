67cb33cc33c7ea9c66cb520f3caeeb48c4a7b0e3
Justin 석원 Choi <justinschoi93@gmail.com>
2024-02-25 20:00:03 -0800

    updated footer styling

--- main/src/components/about/About.jsx
+++ main/src/components/about/About.jsx
@@ -6,7 +6,7 @@ function About () {
     return (
         <section id="about">
             <h5>Get to Know</h5>
-            <h4>About Me</h4>
+            <h2>About Me</h2>
 
             <div className="container about__container">
                 <div className="about__me">
