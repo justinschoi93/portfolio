e5f8510d7854789fd45693185d8716cfe757af7c
Justin <justinschoi93@gmail.com>
2024-03-23 16:43:58 -0700

    built.

---
+++ main/src/components/experience/Experience.jsx
@@ -0,0 +1,273 @@
+import React from "react";
+import "./experience.css";
+import { BsCheck2Circle } from "react-icons/bs";
+
+function Experience () {
+    return (
+        <section id="experience">
+            <h5>Experience</h5>
+            <h2>Acquired Skills</h2>
+
+            <div className="container experience__container">
+                <div className="browser__based__technologies">
+                    <h3>Browser Based Technologies</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>HTML/CSS</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>JavaScript/jQuery</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Responsive Design</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Bootstrap</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Progressive Web Applications (PWAs)</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Local Storage, Session Storage, IndexedDB</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>React.js</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div className="computer__science">
+                    <h3>Computer Science applied to Javascript</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Algorithms (Searches, Sorts)</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Performance</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Time Complexity</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Big O Notation</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Data Structures</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div className="databases">
+                    <h3>Databases</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>MySQL</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>MongoDB</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                
+                <div className="server__side__development">
+                    <h3>Server Side Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>User Authentication</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Template Engines</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>MERN Stack (MongoDB, Express.js, React.js, Node.js)</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div className="api__design">
+                    <h3>Computer Science Applied to Javascript</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Client-Server Model</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>API</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Rest</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>JSON</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>AJAX</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>HTTP request methods</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>GraphQL</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                
+                <div className="deployment__and__delivery">
+                    <h3>Deployment and Delivery</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Heroku</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Git</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>GitHub Pages</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Shell Scripting</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Unit Testing</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Linting</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsCheck2Circle className="experience__details-icon"/>
+                            <div>
+                                <h4>Continuous Integration</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                
+
+            </div>
+        </section>
+    )
+
+}
+
+export default Experience;
