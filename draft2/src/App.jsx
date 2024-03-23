f49300f921e9b47ba12c7e75a45a26305153cb2c
Justin <justinschoi93@gmail.com>
2024-03-23 15:16:06 -0700

    draft2

---
+++ draft2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import About from './components/about/about.jsx';
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App;
---
+++ draft2/src/components/about/about.jsx
@@ -0,0 +1,19 @@
+export default function About() {
+    return (
+        <>
+            <section id="about" className="about">
+                <div className="container about__container">
+                    <div className="about__text">
+                        <h2>About Me</h2>
+                        <p>
+                            I am a full-stack developer with a passion for creating beautiful and functional applications. I have experience working with a variety of technologies including React, Node.js, Express.js, MongoDB and SQL. I am always looking to take on new challenges and improve my skills as a developer.
+                        </p>
+                    </div>
+                    <div className="about__photo">
+                        {/* <img src={Me} alt="image of Justin Choi"></img> */}
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
