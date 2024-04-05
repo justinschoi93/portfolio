4e6f13e17dbd5326a67b39506d92de2948455dba
jOwl <jhome@js-MacBook-Pro.local>
2024-04-04 23:47:04 -0700

    app.css

---
+++ v2.0/src/App.jsx
@@ -0,0 +1,11 @@
+// ============== COMPONENTS ==============
+
+// ================= App =================
+
+export default function App () {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
