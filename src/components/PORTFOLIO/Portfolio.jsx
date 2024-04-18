c830e261023198e58856c40f68dad8266cbe5b16
jOwl <jhome@js-MacBook-Pro.local>
2024-04-18 14:09:46 -0700

    PORTFOLIO: navbar, useStates, useEffect, project display

--- src/components/PORTFOLIO/Portfolio.jsx
+++ src/components/PORTFOLIO/Portfolio.jsx
@@ -1,21 +1,50 @@
-import 'portfolio.css';
+import {useState, useEffect } from 'react';
+import projectsData from './projectsData';
+import './portfolio.css';
 
 const PORTFOLIO = () => {
+    
+    const [active, setActive] = useState('All');
+    const [projects, setProjects] = useState([]);
+    
+    const tabHandler = (e) => {
+        e.preventDefault();
+        setActive(`${e.target.textContent}`);
+    }
+
+    useEffect(()=>{
+        console.log(active);
+        if (active==='All') {
+            setProjects(projectsData);
+
+        } else {
+            setProjects(projectsData.filter( project=> project.type === active))
+        }
+
+        
+    }, [active])
+
+
     return (
         <section className="portfolio__container">
             <div className="portfolio__header">
-                <div className="header__title"></div>
-                <div className="header__subtitle"></div>
+                <h2 className="header__title">PORTFOLIO</h2>
+                <div className="header__subtitle">Things I've made</div>
             </div>
-            <div className="portfolio__navbar">
-                <ul className="portfolio__nav-items">
-                    <li className="nav-item">Project</li>
-                    <li className="nav-item">Project</li>
-                    <li className="nav-item">Project</li>
-                    <li className="nav-item">Project</li>
-                    <li className="nav-item">Project</li>
-                    <li className="nav-item">Project</li>
+            <nav className="portfolio__navbar">
+                <ul className="portfolio__navbar-list">
+                    <li className={`nav-item ${ active  === 'All' ? 'active' : ''}`} onClick={tabHandler}>All</li>
+                    <li className={`nav-item ${ active === 'Full-Stack' ? 'active' : ''}`} onClick={tabHandler}>Full-Stack</li>
+                    <li className={`nav-item ${ active === 'Front-End' ? 'active' : ''}`} onClick={tabHandler}>Front-End</li>
+                    <li className={`nav-item ${ active === 'Back-End' ? 'active' : ''}`} onClick={tabHandler}>Back-End</li>
                 </ul>
+            </nav>
+            <div className="projects__container">
+                {projects.map((project, index)=>(
+                    <div className="project__card" key={index}>
+                        {project.name}
+                    </div>
+                ))}
             </div>
         </section>
     )
---
+++ src/components/PORTFOLIO/projectsData.js
@@ -0,0 +1,29 @@
+const projectsData = [
+    {
+        name: 'Widget Wizard',
+        type: 'Full-Stack',
+        src: 'http://github.com/justinschoi93'
+    },
+    {
+        name: 'Widget Wizard',
+        type: 'Full-Stack',
+        src: 'http://github.com/justinschoi93'
+    },
+    {
+        name: 'Widget Wizard',
+        type: 'Full-Stack',
+        src: 'http://github.com/justinschoi93'
+    },
+    {
+        name: 'Widget Wizard',
+        type: 'Full-Stack',
+        src: 'http://github.com/justinschoi93'
+    },
+    {
+        name: 'Widget Wizard',
+        type: 'Full-Stack',
+        src: 'http://github.com/justinschoi93'
+    },
+]
+
+export default projectsData;
